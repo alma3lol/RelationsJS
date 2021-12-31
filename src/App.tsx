ea4abdbd56df5cf339bfd9517f9e960159fb3d47
Omar Almalol <omar.alma3lol@gmail.com>
2021-12-31 03:07:38 +0200

    feat(i18n): adds translation to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  // use `useTranslation` hook to get the `t` function
+  const { t } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          { t('edit') } <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
