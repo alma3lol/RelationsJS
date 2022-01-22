9ba63db4419d142e0de0cf3658cea777d14fe13a
Omar Almalol <omar.alma3lol@gmail.com>
2022-01-22 16:05:49 +0200

    refactor(models): refactors modeling structure

--- src/models/category.model.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup';
-
-export const CategorySchema = yup.object().shape({
-	id: yup.string().uuid().required(),
-	name: yup.string().required(),
-});
---
+++ src/models/category.model.tsx
@@ -0,0 +1,25 @@
+import { useTranslation } from 'react-i18next';
+import * as yup from 'yup';
+import { ContextMenuItem } from '../neo4j-sigma-graph';
+import {
+	Edit as EditIcon,
+} from '@mui/icons-material';
+
+export const CategorySchema = yup.object().shape({
+	id: yup.string().uuid().required(),
+	name: yup.string().required(),
+});
+
+export const CreateCategoryCypher = `
+CREATE (c:Category {
+	id: $id,
+	name: $name
+})
+`;
+
+export const useCategoryContextMenu = (): ContextMenuItem[] => {
+	const { t } = useTranslation();
+	const menu: ContextMenuItem[] = [];
+	menu.push([<EditIcon />, t('context_menu.category.edit'), id => { console.log('edit', id); }]);
+	return menu;
+}
