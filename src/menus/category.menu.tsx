1aeaba835dc1c308f53a593de87a45b5f9551e46
Omar Almalol <omar.alma3lol@gmail.com>
2022-01-27 23:08:56 +0200

    feat(menus): creates menus folder & refactors category model to seperate menu from model

--- src/menus/category.menu.tsx
+++ src/menus/category.menu.tsx
@@ -3,18 +3,6 @@ import { ContextMenuItem } from '../neo4j-sigma-graph';
 import {
 	Edit as EditIcon,
 } from '@mui/icons-material';
-import { makeAutoObservable } from 'mobx';
-import { v4 } from 'uuid';
-
-export class Category {
-	id = '';
-	name = '';
-	constructor(_id: string = v4()) {
-		makeAutoObservable(this, { id: false });
-		this.id = _id;
-	}
-	setName = (value: string) => this.name = value;
-}
 
 export const useCategoryContextMenu = (): ContextMenuItem[] => {
 	const { t } = useTranslation();
