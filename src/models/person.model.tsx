2e62bbfb3dd6e902f8782afe014125d13e70e5b1
Omar Almalol <omar.alma3lol@gmail.com>
2022-01-27 06:46:37 +0200

    refactor(models): refactors person model to  use mobx

--- src/models/person.model.tsx
+++ src/models/person.model.tsx
@@ -1,70 +1,74 @@
-import * as yup from 'yup';
 import { ContextMenuItem } from '../neo4j-sigma-graph';
 import {
 	Edit as EditIcon,
 } from '@mui/icons-material';
 import { useTranslation } from 'react-i18next';
+import { v4 } from 'uuid';
+import { makeAutoObservable } from 'mobx';
 
-export const PersonSchema = yup.object().shape({
-	id: yup.string().uuid().required(),
-	arabicName: yup.string().required(),
-	englishName: yup.string().required(),
-	motherName: yup.string().required(),
-	nickname: yup.string().nullable(),
-	birthDate: yup.date().nullable(),
-	birthPlace: yup.string().nullable(),
-	job: yup.string().nullable(),
-	nationality: yup.string().required(),
-	phone: yup.string().nullable(),
-	email: yup.string().email().nullable(),
-	workplace: yup.string().nullable(),
-	address: yup.string().nullable(),
-	gpsLocation: yup.string().matches(/\d+\.\d+,\s?\d+\.\d+/, { message: () => ({ key: 'schema.invalid.gps_location', values: { label: 'gpsLocation' } }), excludeEmptyString: true }).nullable(),
-	passportNumber: yup.string().nullable(),
-	passportIssueDate: yup.date().nullable(),
-	passportIssuePlace: yup.string().nullable(),
-	idNumber: yup.string().nullable(),
-	nationalNumber: yup.string().matches(/[12](19|20)[0-9]{9}/, { excludeEmptyString: true }).nullable(),
-	registerationNumber: yup.string().nullable(),
-	restrictions: yup.array().of(yup.string().min(1)).nullable(),
-	notes: yup.string().nullable(),
-	extra: yup.array().of(yup.string().min(1)).nullable(),
-});
-
-export const CreatePersonCypher = `
-CREATE (p:Person {
-	id: $id,
-	arabicName: $arabicName,
-	englishName: $englishName,
-	motherName: $motherName,
-	nickname: $nickname,
-	birthDate: $birthDate,
-	birthPlace: $birthPlace,
-	job: $job,
-	nationality: $nationality,
-	phone: $phone,
-	email: $email,
-	workplace: $workplace,
-	address: $address,
-	gpsLocation: $gpsLocation,
-	passportNumber: $passportNumber,
-	passportIssueDate: $passportIssueDate,
-	passportIssuePlace: $passportIssuePlace,
-	idNumber: $idNumber,
-	nationalNumber: $nationalNumber,
-	registerationNumber: $registerationNumber,
-	restrictions: $restrictions,
-	notes: $notes,
-	extra: $extra
-})
-`;
-
-export const DeletePersonCypher = `
-MATCH (p:Person { id: $id })
-OPTIONAL MATCH (p)-[r]->(o)
-WHERE NOT (r:CATEGORIZED_AS)
-DETACH DELETE r, o, p
-`;
+export class Person {
+	id = '';
+	fileNumber = '';
+	arabicName = '';
+	englishName = '';
+	motherName = '';
+	nickname = '';
+	birthDate = new Date();
+	birthPlace = '';
+	job = '';
+	nationality = '';
+	category = '';
+	phone = '';
+	email = '';
+	workplace = '';
+	address = '';
+	gpsLocation = '';
+	passportNumber = '';
+	passportIssueDate = new Date();
+	passportIssuePlace = '';
+	idNumber = '';
+	nationalNumber = '';
+	registerationNumber = '';
+	restrictions: string[] = [];
+	notes = '';
+	extra: string[] = [];
+	attachments: File[] = [];
+	image: File | null = null;
+	idImage: File | null = null;
+	passportImage: File | null = null;
+	constructor(_id: string = v4()) {
+		makeAutoObservable(this, { id: false });
+		this.id = _id;
+	}
+	setFileNumber = (value: string) => this.fileNumber = value;
+	setArabicName = (value: string) => this.arabicName = value;
+	setEnglishName = (value: string) => this.englishName = value;
+	setMotherName = (value: string) => this.motherName = value;
+	setNickname = (value: string) => this.nickname = value;
+	setBirthDate = (value: Date) => this.birthDate = value;
+	setBirthPlace = (value: string) => this.birthPlace = value;
+	setJob = (value: string) => this.job = value;
+	setNationality = (value: string) => this.nationality = value;
+	setCategory = (value: string) => this.category = value;
+	setPhone = (value: string) => this.phone = value;
+	setEmail = (value: string) => this.email = value;
+	setWorkplace = (value: string) => this.workplace = value;
+	setAddress = (value: string) => this.address = value;
+	setGpsLocation = (value: string) => this.gpsLocation = value;
+	setPassportNumber = (value: string) => this.passportNumber = value;
+	setPassportIssueDate = (value: Date) => this.passportIssueDate = value;
+	setPassportIssuePlace = (value: string) => this.passportIssuePlace = value;
+	setIdNumber = (value: string) => this.idNumber = value;
+	setNationalNumber = (value: string) => this.nationalNumber = value;
+	setRegisterationNumber = (value: string) => this.registerationNumber = value;
+	setRestrictions = (value: string[]) => this.restrictions = value;
+	setNotes = (value: string) => this.notes = value;
+	setExtra = (value: string[]) => this.extra = value;
+	setAttachments = (value: File[]) => this.attachments = value;
+	setImage = (value: File | null) => this.image = value;
+	setIdImage = (value: File | null) => this.idImage = value;
+	setPassportImage = (value: File | null) => this.passportImage = value;
+}
 
 export const usePersonContextMenu = (): ContextMenuItem[] => {
 	const { t } = useTranslation();
