b9c80c4c3513c92976ba573df9a4ad1b60f2c962
Omar Almalol <omar.alma3lol@gmail.com>
2022-01-27 07:16:56 +0200

    refactor(models): refactors media model to use mobx

--- src/models/media.model.tsx
+++ src/models/media.model.tsx
@@ -1,30 +1,25 @@
-import * as yup from 'yup';
 import { useTranslation } from 'react-i18next';
 import { ContextMenuItem } from '../neo4j-sigma-graph';
 import {
 	Edit as EditIcon,
 } from '@mui/icons-material';
+import { FileType } from '../global';
+import { v4 } from 'uuid';
+import { makeAutoObservable } from 'mobx';
 
-export const MediaSchema = yup.object().shape({
-	id: yup.string().required(),
-	name: yup.string().required(),
-	path: yup.string().required(),
-	type: yup.mixed().oneOf(['image', 'video', 'id', 'avatar', 'passport', 'attachment']).required(),
-});
-
-export const CreateMediaCypher = `
-CREATE (m:Media {
-	id: $id,
-	name: $name,
-	path: $path,
-	type: $type
-})
-`;
-
-export const DeleteMediaCypher = `
-MATCH (m:Media { id: $id })
-DETACH DELETE m
-`;
+export class Media {
+	id = '';
+	name = '';
+	path = '';
+	type: FileType = 'image';
+	constructor(_id: string = v4()) {
+		makeAutoObservable(this, { id: false });
+		this.id = _id;
+	}
+	setName = (value: string) => this.name = value;
+	setPath = (value: string) => this.path = value;
+	setType = (value: FileType) => this.type = value;
+}
 
 export const useMediaContextMenu = () => {
 	const { t } = useTranslation();
