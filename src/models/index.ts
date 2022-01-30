f4e438545fc73f71694a95594d6b4695f3a3b994
Omar Almalol <omar.alma3lol@gmail.com>
2022-01-30 02:04:05 +0200

    feat(models): adds record model

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export * from './category.model';
 export * from './person.model';
 export * from './media.model';
 export * from './nationality.model';
+export * from './record.model';
---
+++ src/models/record.model.ts
@@ -0,0 +1,16 @@
+import { makeAutoObservable } from "mobx";
+import { v4 } from "uuid";
+
+export class Record {
+	id = '';
+	title = '';
+	text = '';
+	date: Date | null = null;
+	constructor(id: string = v4()) {
+		makeAutoObservable(this, { id: false });
+		this.id = id;
+	}
+	setTitle = (title: string) => this.title = title;
+	setText = (text: string) => this.text = text;
+	setDate = (date: Date | null) => this.date = date;
+}
