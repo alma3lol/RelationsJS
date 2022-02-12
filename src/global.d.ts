8d7d98619ca750a6420c1ea8a188c11329b6940d
Omar Almalol <omar.alma3lol@gmail.com>
2022-02-12 04:01:41 +0200

    fix(global): corrects content type and adds getFile method

--- src/global.d.ts
+++ src/global.d.ts
@@ -11,11 +11,15 @@ declare global {
 		 * Functions in 'public/preload.js'
 		 */
 		files: {
-			upload: (nodeId: string, fileType: FileType, filename: string, content: any) => string
+			upload: (nodeId: string, fileType: FileType, filename: string, content: ArrayBuffer) => string
 			list: (nodeId: string, filesType: FileType) => string[]
 			delete: (nodeId: string, fileType: FileType, filename: string) => void
 			clearUnused: (filesType: FileType, files: any[]) => Pomise<number>
 			saveFile: (content, title?: string) => Pomise<boolean>
+			getFile: (filePath) => File
+		},
+		electron: {
+			print: (filename: string, title?: string) => Promise<void>;
 		}
 	}
 }
