b27b1a84c135214c72c944eeeb7e249b7f02c905
Omar Almalol <omar.alma3lol@gmail.com>
2022-02-13 18:30:56 +0200

    refactor(views): refactors list view to enhance search & hotkeys

--- src/views/list.view.tsx
+++ src/views/list.view.tsx
@@ -1,28 +1,61 @@
-import { AppBar, Card, CardContent, CardMedia, Grid, IconButton, MenuItem, Paper, Theme, Toolbar, Typography, Select, TextField, InputLabel, FormControl, OutlinedInput, CardActionArea, Tooltip } from "@mui/material";
+import {
+	AppBar,
+	Card,
+	CardContent,
+	CardMedia,
+	Grid,
+	IconButton,
+	MenuItem,
+	Paper,
+	Theme,
+	Toolbar,
+	Typography,
+	Select,
+	TextField,
+	InputLabel,
+	FormControl,
+	OutlinedInput,
+	CardActionArea,
+	Tooltip,
+	CardActions,
+    Collapse,
+    FormGroup,
+	FormControlLabel,
+	Checkbox,
+} from "@mui/material";
 import { DefaultTheme, makeStyles } from "@mui/styles";
 import useHotkeys from "@reecelucas/react-use-hotkeys";
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Category, Nationality, Person, Transcript } from "../models";
+import { Person, Transcript } from "../models";
 import { Neo4jSigmaGraph } from "../neo4j-sigma-graph";
 import {
 	Settings as SettingsIcon,
 	Add as AddIcon,
-	SavedSearch as SavedSearchIcon,
+	Print as PrintIcon,
+	Edit as EditIcon,
+	ExpandMore as ExpandMoreIcon,
+	LocalOffer as LocalOfferIcon,
+	Flag as FlagIcon,
 } from '@mui/icons-material';
 import { useTranslation } from "react-i18next";
 import { appContext } from "../App";
-import { AddNode, QuickFind, Settings } from "../components";
+import { AddNode, Categories, Nationalities, Settings } from "../components";
 import { PersonView } from "./person.view";
 import PersonSvgIcon from '../images/person.svg';
 import PersonLightSvgIcon from '../images/person-light.svg';
+import { ArticleDark, ArticleLight } from "../images";
+import { TranscriptView } from "./transcript.view";
 
 const useStyles = makeStyles<DefaultTheme, Theme, string>({
 	appBar: {
 		direction: "ltr",
 	},
 	paper: {
-		height: (theme) => `calc(100% - ${theme.spacing(20)} - ${theme.mixins.toolbar.minHeight}px) !important`,
+		transition: theme => `${theme.transitions.create(["height"], {
+			duration: theme.transitions.duration.standard,
+			easing: theme.transitions.easing.easeInOut,
+		})} !important`,
 		overflow: "auto",
 		"&::-webkit-scrollbar": {
 			width: "0 !important",
@@ -35,6 +68,7 @@ export const ListView = () => {
 	const { language, theme, darkMode } = useContext(appContext);
 	const classes = useStyles(theme);
 	const [people, setPeople] = useState<Person[]>([]);
+	const [transcripts, setTranscripts] = useState<Transcript[]>([]);
 	const fetchPeople = async () => {
 		try {
 			const repo = Neo4jSigmaGraph.getInstance().getRepository('PERSON');
@@ -45,43 +79,90 @@ export const ListView = () => {
 		} catch (error) {
 			console.error(error);
 		}
-	};
+	}
+	const fetchTranscripts = async () => {
+		try {
+			const repo = Neo4jSigmaGraph.getInstance().getRepository('TRANSCRIPT');
+			if (repo) {
+				const transcripts = await repo.read();
+				setTranscripts(transcripts);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
 	useEffect(() => {
 		fetchPeople();
+		fetchTranscripts();
 	}, []);
 	const navigate = useNavigate();
 	useHotkeys('Control+Tab', () => {
 		navigate('/');
 	});
 	const [filteredPeople, setFilteredPeople] = useState<Person[]>([]);
+	const [filteredTranscripts, setFilteredTranscripts] = useState<Transcript[]>([]);
 	const [search, setSearch] = useState<string>('');
 	useEffect(() => {
-		setFilteredPeople(people.filter(person => person.arabicName.toLowerCase().includes(search.toLowerCase())));
-	}, [search, people]);
+		setFilteredPeople(people.filter(person => {
+			let result = false;
+			if (
+				person.arabicName.toLowerCase().includes(search.toLowerCase()) ||
+				person.englishName.toLowerCase().includes(search.toLowerCase()) ||
+				person.motherName.toLowerCase().includes(search.toLowerCase()) ||
+				person.nickname.toLowerCase().includes(search.toLowerCase()) ||
+				person.category.label.toLowerCase().includes(search.toLowerCase()) ||
+				person.nationality.label.toLowerCase().includes(search.toLowerCase()) ||
+				person.job.toLowerCase().includes(search.toLowerCase()) ||
+				person.email.toLowerCase().includes(search.toLowerCase()) ||
+				person.phone.toLowerCase().includes(search.toLowerCase()) ||
+				person.notes.toLowerCase().includes(search.toLowerCase()) ||
+				person.address.toLowerCase().includes(search.toLowerCase()) ||
+				person.gpsLocation.toLowerCase().includes(search.toLowerCase()) ||
+				person.fileNumber.toLowerCase().includes(search.toLowerCase()) ||
+				person.workplace.toLowerCase().includes(search.toLowerCase()) ||
+				person.idNumber.toLowerCase().includes(search.toLowerCase()) ||
+				person.passportNumber.toLowerCase().includes(search.toLowerCase()) ||
+				person.passportIssuePlace.toLowerCase().includes(search.toLowerCase()) ||
+				person.birthPlace.toLowerCase().includes(search.toLowerCase()) ||
+				person.nationalNumber.toLowerCase().includes(search.toLowerCase())
+			) result = true;
+			person.extra.forEach(extra => {
+				if (extra.toLowerCase().includes(search.toLowerCase())) result = true;
+			});
+			person.restrictions.forEach(restriction => {
+				if (restriction.toLowerCase().includes(search.toLowerCase())) result = true;
+			});
+			return result;
+		}));
+		setFilteredTranscripts(transcripts.filter(transcript => {
+			let result = false;
+			if (
+				transcript.title.toLowerCase().includes(search.toLowerCase()) ||
+				transcript.content.toLowerCase().includes(search.toLowerCase())
+			) result = true;
+			transcript.mentioned.forEach(mentioned => {
+				if (mentioned.label.toLowerCase().includes(search.toLowerCase())) result = true;
+			});
+			return result;
+		}));
+	}, [search, people, transcripts]);
 	const [showSettings, setShowSettings] = useState(false);
 	const [showAddNode, setShowAddNode] = useState(false);
-	const [quickFind, setQuickFind] = useState(false);
-	useHotkeys('Control+f', () => setQuickFind(true));
 	useHotkeys('Escape', () => {
 		setShowAddNode(false);
 		setShowSettings(false);
-		setQuickFind(false);
 		setViewedPerson(null);
+		setViewedTranscript(null);
 	}, true);
-	useHotkeys('Control+n', e => {
+	useHotkeys(['Control+n', 'Control+ى'], e => {
 		e.preventDefault();
 		setShowAddNode(true);
 		setShowSettings(false);
-		setQuickFind(false);
 	});
-	useHotkeys('Control+s', () => {
+	useHotkeys(['Control+s', 'Control+س'], () => {
 		setShowAddNode(false);
 		setShowSettings(true);
-		setQuickFind(false);
 	});
-	const handleQuickFindResult = (nodes: (Person | Category | Transcript | Nationality)[]) => {
-		setPeople(nodes.filter(node => node instanceof Person) as Person[]);
-	}
 	type PersonSortKeys = 'arabicName' | 'englishName' | 'motherName' | 'nickname' | 'job' | 'address' | 'birthDate' | 'birthPlace' | 'workplace' | 'phone' | 'email' | 'gpsLocation' | 'passportNumber' | 'nationalNumber' | 'idNumber' | 'registerationNumber' | 'category' | 'nationality';
 	type SortKeys = PersonSortKeys;
 	const [sortBy, setSortBy] = useState<SortKeys>('arabicName');
@@ -210,6 +291,18 @@ export const ListView = () => {
 		nationality: t('forms.inputs.person.nationality'),
 	}
 	const [viewedPerson, setViewedPerson] = useState<Person | null>(null);
+	const [viewedTranscript, setViewedTranscript] = useState<Transcript | null>(null);
+	const handleOnMentionedClick = (id: string) => {
+		const person = people.find(p => p.id === id);
+		if (person) {
+			setViewedPerson(person);
+		}
+	}
+	const [showAdvacedFiltering, setShowAdvacedFiltering] = useState(false);
+	const [showPeople, setShowPeople] = useState(true);
+	const [showTranscripts, setShowTranscripts] = useState(true);
+	const [showNationalities, setShowNationalities] = useState(false);
+	const [showCategories, setShowCategories] = useState(false);
 	return (
 		<>
 			<AppBar position="static" enableColorOnDark className={classes.appBar} color='primary'>
@@ -223,9 +316,14 @@ export const ListView = () => {
 							</Tooltip>
 						</Grid>
 						<Grid item>
-							<Tooltip title={t('quick_find.title')}>
-								<IconButton color="inherit" aria-label="menu" onClick={() => setQuickFind(true)}>
-									<SavedSearchIcon />
+							<Tooltip title={t('forms.titles.nationalities')}>
+								<IconButton color="inherit" aria-label="menu" onClick={() => setShowNationalities(true)}>
+									<FlagIcon />
+								</IconButton>
+							</Tooltip>
+							<Tooltip title={t('forms.titles.categories')}>
+								<IconButton color="inherit" aria-label="menu" onClick={() => setShowCategories(true)}>
+									<LocalOfferIcon />
 								</IconButton>
 							</Tooltip>
 							<Tooltip title={t('forms.add_node.title')}>
@@ -258,28 +356,67 @@ export const ListView = () => {
 							</Select>
 						</FormControl>
 					</Grid>
-					<Grid item xs={3}>
-						<FormControl fullWidth>
-							<InputLabel id="orderBy" shrink>{t('forms.orderBy')}</InputLabel>
-							<Select value={orderBy} onChange={e => setOrderBy((e.target.value) as OrderBy)} input={<OutlinedInput label={t('forms.orderBy')} id='orderBy' />}>
-								<MenuItem value={'ASC'}>{t('forms.ascendingOrder')}</MenuItem>
-								<MenuItem value={'DESC'}>{t('forms.descendingOrder')}</MenuItem>
-							</Select>
-						</FormControl>
+					<Grid item xs={3} container spacing={2}>
+						<Grid item xs={10}>
+							<FormControl fullWidth>
+								<InputLabel id="orderBy" shrink>{t('forms.orderBy')}</InputLabel>
+								<Select value={orderBy} onChange={e => setOrderBy((e.target.value) as OrderBy)} input={<OutlinedInput label={t('forms.orderBy')} id='orderBy' />}>
+									<MenuItem value={'ASC'}>{t('forms.ascendingOrder')}</MenuItem>
+									<MenuItem value={'DESC'}>{t('forms.descendingOrder')}</MenuItem>
+								</Select>
+							</FormControl>
+						</Grid>
+						<Grid item xs='auto' mt={1}>
+							<Tooltip title={t('forms.advanced')}>
+								<IconButton onClick={() => setShowAdvacedFiltering(!showAdvacedFiltering)}>
+									<ExpandMoreIcon sx={{ transform: showAdvacedFiltering ? 'rotate(180deg)' : 'rotate(0deg)', transition: theme.transitions.create('transform', { duration: theme.transitions.duration.shortest }) }} />
+								</IconButton>
+							</Tooltip>
+						</Grid>
+					</Grid>
+<Grid item xs={12} sx={{ pt: '0px !important' }}>
+						<Collapse in={showAdvacedFiltering}>
+<FormGroup row sx={{ mt: 2 }}>
+								<FormControlLabel control={<Checkbox checked={showPeople} onChange={e => setShowPeople(e.target.checked)} />} label={t('forms.showPeople') as string} />
+								<FormControlLabel control={<Checkbox checked={showTranscripts} onChange={e => setShowTranscripts(e.target.checked)} />} label={t('forms.showTranscripts') as string} />
+							</FormGroup>
+						</Collapse>
 					</Grid>
 				</Grid>
 			</Paper>
-			<Paper sx={{ p: 2, m: 2 }} elevation={3} className={classes.paper}>
+			<Paper sx={{ p: 2, m: 2, height: `calc(100% - ${theme.spacing(20 + (showAdvacedFiltering ? 8 : 0))} - ${theme.mixins.toolbar.minHeight}px) !important`, }} elevation={3} className={classes.paper}>
 				<Grid container spacing={2}>
-					{filteredPeople.map(person => (
+					{showPeople && filteredPeople.map(person => (
 						<Grid item xs={3} sm={3} md={3} key={person.id}>
 							<Card>
 								<CardActionArea onClick={() => setViewedPerson(person)}>
+									<CardMedia height='400' component='img' sx={{ mb: 1 }} image={person.image ? URL.createObjectURL(person.image) : darkMode ? PersonSvgIcon : PersonLightSvgIcon} />
 									<CardContent>
-										<CardMedia height='400' component='img' sx={{ mb: 1 }} image={person.image ? URL.createObjectURL(person.image) : darkMode ? PersonSvgIcon : PersonLightSvgIcon} />
-										<Typography color="textSecondary">{person.arabicName}</Typography>
+										<Typography noWrap variant='h5'>{person.arabicName}</Typography>
+										<Typography variant='caption' component='i'>{person.category.label}</Typography>
 									</CardContent>
 								</CardActionArea>
+								<CardActions>
+									<Tooltip title={t('edit')}><IconButton onClick={() => navigate(`/edit/person/${person.id}`)}><EditIcon /></IconButton></Tooltip>
+									<Tooltip title={t('print')}><IconButton onClick={() => navigate(`/print/person/${person.id}`)}><PrintIcon /></IconButton></Tooltip>
+								</CardActions>
+							</Card>
+						</Grid>
+					))}
+					{showTranscripts && filteredTranscripts.map(transcript => (
+						<Grid item xs={3} sm={3} md={3} key={transcript.id}>
+							<Card>
+								<CardActionArea onClick={() => setViewedTranscript(transcript)}>
+									<CardMedia height='400' component='img' sx={{ mb: 1 }} image={darkMode ? ArticleLight : ArticleDark} />
+									<CardContent>
+										<Typography noWrap variant='h5'>{transcript.title}</Typography>
+										<Typography noWrap variant='caption' component='i'>{transcript.content}</Typography>
+									</CardContent>
+								</CardActionArea>
+								<CardActions>
+									<Tooltip title={t('edit')}><IconButton onClick={() => navigate(`/edit/transcript/${transcript.id}`)}><EditIcon /></IconButton></Tooltip>
+									<Tooltip title={t('print')}><IconButton onClick={() => navigate(`/print/transcript/${transcript.id}`)}><PrintIcon /></IconButton></Tooltip>
+								</CardActions>
 							</Card>
 						</Grid>
 					))}
@@ -287,8 +424,10 @@ export const ListView = () => {
 			</Paper>
 			<Settings show={showSettings} close={() => setShowSettings(false)} onDone={() => setShowSettings(false)} />
 			<AddNode show={showAddNode} onDone={fetchPeople} close={() => setShowAddNode(false)} />
-			<QuickFind show={quickFind} onClose={() => setQuickFind(false)} onDone={handleQuickFindResult} />
+			<Categories onDone={fetchPeople} open={showCategories} onClose={() => setShowCategories(false)} />
+			<Nationalities onDone={fetchPeople} open={showNationalities} onClose={() => setShowNationalities(false)} />
 			{viewedPerson && <PersonView show={viewedPerson !== null} person={viewedPerson} onClose={() => setViewedPerson(null)} />}
+			{viewedTranscript && <TranscriptView show={viewedTranscript !== null} transcript={viewedTranscript} onClose={() => setViewedTranscript(null)} onMentionedClick={handleOnMentionedClick} />}
 		</>
 	);
 };
